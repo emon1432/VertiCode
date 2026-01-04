d5f566a63cda8c6b4265186542e3acf5bf0a4c32
emon1432 <ovirajemon143@gmail.com>
2026-01-04 22:22:09 +0600

    feat: add Platform model and migration for platforms table

---
+++ app/Models/Platform.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Platform extends Model
+{
+    protected $fillable = [
+        'name',
+        'display_name',
+        'base_url',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+}
